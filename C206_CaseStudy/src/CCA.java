989be2d06763f97b77e75c89591c8059db42c8e9
20045435 <20045435@LAPTOP-K2L14UA6>
2021-08-03 22:53:58 +0800

    done by faith

---
+++ C206_CaseStudy/src/CCA.java
@@ -0,0 +1,78 @@
+// done by faith
+public class CCA {
+	
+	private String title;
+	private String description;
+	private int classSize;
+	private String dayOfWeek;
+	private String time;
+	private String venue;
+	private String instructorName;
+	
+	public CCA(String title, String description, int classSize, String dayOfWeek, String time, String venue,
+			String instructorName) {
+		this.title = title;
+		this.description = description;
+		this.classSize = classSize;
+		this.dayOfWeek = dayOfWeek;
+		this.time = time;
+		this.venue = venue;
+		this.instructorName = instructorName;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getClassSize() {
+		return classSize;
+	}
+
+	public void setClassSize(int classSize) {
+		this.classSize = classSize;
+	}
+
+	public String getDayOfWeek() {
+		return dayOfWeek;
+	}
+
+	public void setDayOfWeek(String dayOfWeek) {
+		this.dayOfWeek = dayOfWeek;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getVenue() {
+		return venue;
+	}
+
+	public void setVenue(String venue) {
+		this.venue = venue;
+	}
+
+	public String getInstructorName() {
+		return instructorName;
+	}
+
+	public void setInstructorName(String instructorName) {
+		this.instructorName = instructorName;
+	}
+}
