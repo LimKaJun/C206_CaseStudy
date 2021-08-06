db93c32c96bb621cca2fa34ad7f94c36815f01f4
20033824 <20033824@myrp.edu.sg>
2021-08-06 10:14:43 +0800

    new requirement test

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -79,6 +79,44 @@ public class C206_CaseStudyTest {
 		assertEquals(0, ParentAccountList.size());
 
 	}
+	
+	@Test
+	public void testUpdateParentAccount() {
+		assertNotNull(ParentAccountList);
+		
+		C206_CaseStudy.addParentAccount(parentAccount1, ParentAccountList);
+		C206_CaseStudy.addParentAccount(parentAccount2, ParentAccountList);
+		
+		C206_CaseStudy.updateParentAccountInt(1, 1, ParentAccountList, 123);
+		assertEquals(123,parentAccount1.getStudentId());
+		C206_CaseStudy.updateParentAccountString(2, 1, ParentAccountList, "123");
+		assertEquals("123",parentAccount1.getName());
+		
+		C206_CaseStudy.updateParentAccountInt(3, 2, ParentAccountList, 123);
+		assertEquals(123,parentAccount2.getGrade());
+		C206_CaseStudy.updateParentAccountChar(4, 2, ParentAccountList, 'A');
+		assertEquals('A',parentAccount2.getClassLetter());
+		C206_CaseStudy.updateParentAccountString(5, 2, ParentAccountList, "123");
+		assertEquals("123",parentAccount2.getTeacher());
+		C206_CaseStudy.updateParentAccountString(6, 2, ParentAccountList, "123");
+		assertEquals("123",parentAccount2.getParentName());
+		C206_CaseStudy.updateParentAccountString(7, 2, ParentAccountList, "123");
+		assertEquals("123",parentAccount2.getParentEmail());
+		C206_CaseStudy.updateParentAccountString(8, 2, ParentAccountList, "123");
+		assertEquals("123",parentAccount2.getParentAddress());
+		C206_CaseStudy.updateParentAccountInt(9, 2, ParentAccountList,123);
+		assertEquals(123,parentAccount2.getParentContact());
+		C206_CaseStudy.updateParentAccountInt(10, 2, ParentAccountList, 123);
+		assertEquals(123,parentAccount2.getRegistrationID());
+		C206_CaseStudy.updateParentAccountString(11, 123, ParentAccountList, "123");
+		assertEquals("123",parentAccount2.getCcaRegistered());
+		String allParentAccount = C206_CaseStudy.retrieveAllParentAccount(ParentAccountList);
+		String test = String.format("%-15d %-15s %-10d %-10c %-15s %-15s %-20s %-30s %-15d %-20d %-15s \n", 123, "123",
+				12345, 'C', "Desmond", "Mary", "mary@gmail.com", "Pasir Ris Avenue 3 520511", 91234567, 1, "NONE");
+		test += String.format("%-15d %-15s %-10d %-10c %-15s %-15s %-20s %-30s %-15d %-20d %-15s \n", 1, "Aseerah",
+				123, 'A', "123", "123", "123", "123", 123, 123, "123");
+		assertEquals(test, allParentAccount);
+	}
 
 }
 //testing 123 please comment below if you receive this
